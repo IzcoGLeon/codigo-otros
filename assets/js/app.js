87dfe2e8e43537ffd31942cc8e68cd8398191833
IzcoGLeon <Izco.g.leon@outlook.com>
2023-11-27 16:38:53 -0600

    modified css, js and index

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,33 @@
+//se cambian los nombres de las constantes a una convención camelToe para que sea mas entendible
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
-const $l = document.querySelector('.location');
+const nameRef = document.querySelector('.name'); //Se corrigen referencias al atributo de clase 
+const blogRef = document.querySelector('.blog');
+const locationRef = document.querySelector('.location');
 
-function displayUser(username) {
-  $n.textContent = 'cargando...';
-  const response = await fetch(`${usersEndpoint}/${username}`);
-  console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+//Se agregó la función apra solicitud async ya que await no puede funcionar sin una declaración con async
+
+async function displayUser(username) {
+  nameRef.textContent = 'cargando...';
+
+  //Necesario manejar excepciones y la solicitud fetch con try-catch
+
+  try {
+    const response = await fetch(`${usersEndpoint}/${username}`);
+    const data = await response.json(); //Se convierte en objeto el texto plano
+    console.log(data);
+    nameRef.textContent = `${data.name}`;
+    blogRef.textContent = `${data.blog}`; //Se corrige la apostrofe para uso de template literals
+    locationRef.textContent = `${data.location}`;
+  } catch (error) {
+    handleError(error)
+  }
 }
 
 function handleError(err) {
   console.log('OH NO!');
   console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+  nameRef.textContent = `Algo salió mal: ${err}`
 }
 
-displayUser('stolinski').catch(handleError);
+displayUser('stolinski');
