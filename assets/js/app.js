e212aaca71588ccc2757f42b5a4884059dcbc66f
IzcoGLeon <Izco.g.leon@outlook.com>
2023-11-27 19:54:12 -0600

    Did changes on html, styles.css and app.js files to make the code work and run

--- assets/js/app.js
+++ assets/js/app.js
@@ -20,7 +20,7 @@ async function displayUser(username) {
     blogRef.textContent = `${data.blog}`; //Se corrige la apostrofe para uso de template literals
     locationRef.textContent = `${data.location}`;
   } catch (error) {
-    handleError(error)
+    handleError(error); //Se coloca función que manejara la excepción de error dentro del bloque catch
   }
 }
 
@@ -30,4 +30,10 @@ function handleError(err) {
   nameRef.textContent = `Algo salió mal: ${err}`
 }
 
-displayUser('stolinski');
+// displayUser('stolinski');
+
+//Se crea una función para que se pueda ingresar cualquer otro usuario a través de un campo de texto, esta acción y recopilación de datos y referencias se genera a través de la acción onclick del elemento button en el documento HTML
+const getUsername = user => {
+  const userRef = document.getElementById('user').value;
+  displayUser(userRef);
+}
